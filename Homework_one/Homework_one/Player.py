23fb6e42a31adb5104e765c2bc3f024e5bb8c78a
DESKTOP-EQ67UDA\Chad <ccredeur@uccs.edu>
2019-03-04 14:38:13 -0700

    Commented-out player input block

--- Homework_one/Homework_one/Player.py
+++ Homework_one/Homework_one/Player.py
@@ -7,12 +7,12 @@ from Vector import Vector
 class Player(Agent):
 
     def update(self, screenBounds, graph, herd, GATES):
-        pressed = pygame.key.get_pressed()
-        self.velocity = Vector(0, 0)
-        if pressed[pygame.K_a]: self.velocity.x = -1
-        if pressed[pygame.K_d]: self.velocity.x = 1
-        if pressed[pygame.K_w]: self.velocity.y = -1
-        if pressed[pygame.K_s]: self.velocity.y = 1
-        if self.velocity != Vector(0,0):
-            self.currentSpeed = Constants.DOG_SPEED
+        #pressed = pygame.key.get_pressed()
+        #self.velocity = Vector(0, 0)
+        #if pressed[pygame.K_a]: self.velocity.x = -1
+        #if pressed[pygame.K_d]: self.velocity.x = 1
+        #if pressed[pygame.K_w]: self.velocity.y = -1
+        #if pressed[pygame.K_s]: self.velocity.y = 1
+        #if self.velocity != Vector(0,0):
+            #self.currentSpeed = Constants.DOG_SPEED
         super(Player, self).update(screenBounds, graph, herd, GATES)
